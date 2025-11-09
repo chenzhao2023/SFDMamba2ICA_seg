176fbc437e8fe6847a4c67cc096c8659527b0460
LonelySRQ <srq010920@Gmail.com>
2025-11-10 00:59:25 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,88 +1,139 @@
-from torch import nn, optim
-import torch
 import os
+import torch
+from torch import nn, optim
 from torch.utils.data import DataLoader
-from data import *
-from torchvision.utils import save_image
 from net import *
+from data import PairedDataset
 
-device=torch.device('cuda:0'if torch.cuda.is_available() else 'cpu')     # todo cuda:0, cuda:1
-weight_path=r'weight'
-data_path=r'dataset/train'
-save_path=r'train_image'
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+weight_path = "weights"
+data_path_s = r"dataset/source"
+data_path_t = r"dataset/target"
+
+epochs = 200
+batch_size = 1
+lr = 0.001
+lambda_sda = 0.03
+
+alpha, beta = 0.7, 0.3
+bce_loss_seg = nn.BCEWithLogitsLoss()
+mse_loss = nn.MSELoss()
+
+
+def segmentation_loss(pred, mask, alpha=alpha, beta=beta):
+    loss_bce = bce_loss_seg(pred, mask)
+    loss_mse = mse_loss(torch.sigmoid(pred), mask)
+    return alpha * loss_bce + beta * loss_mse
+
+
+bce_loss = nn.BCEWithLogitsLoss()
+
 
 def dice_coefficient(pred, target, smooth=1e-10):
-    pred[pred >= 0.5] = 1
-    pred[pred < 0.5] = 0
-    pred = pred.view(-1)
-    target = target.view(-1)
+    pred = (pred >= 0.5).float()
+    pred, target = pred.view(-1), target.view(-1)
     intersection = (pred * target).sum()
     return (2. * intersection + smooth) / (pred.sum() + target.sum() + smooth)
 
-def val(data):
-    for j in range(image.size(0)):
-        output_image_name = segment_name[j]
-        output_image_path = os.path.join(save_path, output_image_name)
-        save_image(out_image[j], output_image_path)
-
-
-class MSELoss(nn.Module):
-    def __init__(self):
-        super(MSELoss, self).__init__()
-
-    def forward(self, pred, target):
-        return F.mse_loss(pred, target)
 
 def save_checkpoint(epoch, model, optimizer):
-    checkpoint = {
+    os.makedirs(weight_path, exist_ok=True)
+    torch.save({
         'model_state_dict': model.state_dict(),
-        'optimizer_state_dict': optimizer.state_dict(),
-    }
-    torch.save(checkpoint, f'weight/{epoch}.pth')
-
-if __name__ == '__main__':
-    data_loader=DataLoader(MyDataset(data_path), batch_size=2, shuffle=True)
-    net=SFD_Mamba2Net().to(device)
-    chekpoint = False
-    chek=''
-    if chekpoint:
-        net.load_state_dict(torch.load(f'weight/{chek}.pth'))
-        print('successful loading weight')
-
-    opt = optim.Adam(net.parameters(), lr=0.0001)
-    scheduler = torch.optim.lr_scheduler.StepLR(opt, step_size=10, gamma=0.5)
-    loss_fun = MSELoss()
-    total_train_dice = 0.0
-    total_epoch_count = 0
-
-    epoch=1
-    while (epoch<=800):
-        total_dice = 0.0
-        total_batches = 0
-        for i,(image,segment_image,segment_name) in enumerate(data_loader):
-            image, segment_image,=image.to(device),segment_image.to(device)
-            out_image = net(image)
-            train_loss = loss_fun(out_image, segment_image)
-            opt.zero_grad()
-            train_loss.backward()
-            opt.step()
-
-            dice = dice_coefficient(out_image, segment_image)
-            total_dice += dice.item()
-            total_batches += 1
-
-            if i%100==0:
-                print(f'{epoch}--{i}--train_loss==>{train_loss.item()}--dice={dice.item()}')
-        if (epoch % 5) == 0:
-            save_checkpoint(epoch,net, opt)
-            torch.save(net.state_dict(), f'weight/{epoch}.pth')
+        'optimizer_state_dict': optimizer.state_dict()
+    }, f'{weight_path}/epoch_{epoch}.pth')
 
 
-        average_dice = total_dice / total_batches if total_batches > 0 else 0.0
-        print(f'Epoch {epoch} - Average Dice: {average_dice}')
+def multi_stage_lr(epoch):
+    if epoch < 10:
+        return 1.0
+    elif epoch < 20:
+        return 0.5
+    elif epoch < 30:
+        return 0.25
+    else:
+        return 0.3125
 
-        scheduler.step()
-        epoch+=1
 
-    final_average_dice = total_train_dice / total_epoch_count if total_epoch_count > 0 else 0.0
-    print(f'Training Finished - 平均dice= {final_average_dice}')
+paired_dataset = PairedDataset(
+    source_img_dir=os.path.join(data_path_s, "images"),
+    source_label_dir=os.path.join(data_path_s, "masks"),
+    target_img_dir=os.path.join(data_path_t, "images"),
+    target_label_dir=os.path.join(data_path_t, "masks")
+)
+
+loader = DataLoader(paired_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+net = SFD_Mamba2Net().to(device)
+
+opt = optim.Adam(
+    net.parameters(),
+    lr=1e-4,
+    betas=(0.9, 0.999),
+    weight_decay=1e-5
+)
+scheduler = torch.optim.lr_scheduler.LambdaLR(opt, lr_lambda=multi_stage_lr)
+
+checkpoint = False
+chek = ''
+if checkpoint and chek:
+    net.load_state_dict(torch.load(f'{weight_path}/{chek}.pth')['model_state_dict'])
+    print(f'Successfully loaded weight: {chek}')
+
+for epoch in range(1, epochs + 1):
+    net.train()
+    total_dice_s, total_dice_t = 0., 0.
+    total_loss_s, total_loss_t, total_loss_sda, batches = 0., 0., 0., 0
+
+    current_lambda_sda = lambda_sda
+
+    for i, (img_s, mask_s, _, img_t, mask_t, _) in enumerate(loader):
+        img_s, mask_s = img_s.to(device), mask_s.to(device)
+        img_t, mask_t = img_t.to(device), mask_t.to(device)
+        B = img_s.size(0)
+
+        img = torch.cat([img_s, img_t], dim=0)
+        mask = torch.cat([mask_s, mask_t], dim=0)
+
+        pred, dom_out = net(img, lambd=current_lambda_sda, cond=mask)
+
+        pred_s, pred_t = pred[:B], pred[B:]
+        mask_s, mask_t = mask[:B], mask[B:]
+
+        loss_s = segmentation_loss(pred_s, mask_s)
+        loss_t = segmentation_loss(pred_t, mask_t)
+
+        label_s = torch.zeros(B, 1, device=device)  # source=0
+        label_t = torch.ones(B, 1, device=device)  # target=1
+        labels = torch.cat([label_s, label_t], dim=0)
+        loss_sda = bce_loss(dom_out, labels)
+
+        loss = loss_s + loss_t + current_lambda_sda * loss_sda
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        dice_s = dice_coefficient(torch.sigmoid(pred_s), mask_s).item()
+        dice_t = dice_coefficient(torch.sigmoid(pred_t), mask_t).item()
+
+        total_dice_s += dice_s
+        total_dice_t += dice_t
+        total_loss_s += loss_s.item()
+        total_loss_t += loss_t.item()
+        total_loss_sda += loss_sda.item()
+        batches += 1
+
+        if i % 50 == 0:
+            print(f'Epoch {epoch} | Iter {i} | '
+                  f'LossS={loss_s.item():.4f} LossT={loss_t.item():.4f} LossSDA={loss_sda.item():.4f} '
+                  f'| DiceS={dice_s:.4f} DiceT={dice_t:.4f} | LambdaSDA={current_lambda_sda:.3f}')
+
+    print(f'Epoch {epoch} Summary | '
+          f'AvgLossS={total_loss_s / batches:.4f} AvgLossT={total_loss_t / batches:.4f} AvgLossSDA={total_loss_sda / batches:.4f} '
+          f'| AvgDiceS={total_dice_s / batches:.4f} AvgDiceT={total_dice_t / batches:.4f} | LambdaSDA={current_lambda_sda:.3f}')
+
+    if epoch % 5 == 0:
+        save_checkpoint(epoch, net, opt)
+
+    scheduler.step()
